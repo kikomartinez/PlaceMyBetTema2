2b0ca61dcae685dd0c9a82a02c85a0b0180d644e
quicomartinez11 <elok11@gmail.com>
2020-09-23 16:20:00 +0200

    UsersRepository conecta correctamente con SQL

--- AplicacionWebAspNet/PlaceMyBet/PlaceMyBet/Models/UsersRepository.cs
+++ AplicacionWebAspNet/PlaceMyBet/PlaceMyBet/Models/UsersRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,34 @@ namespace PlaceMyBet.Models
 {
     public class UsersRepository
     {
+
+        private MySqlConnection Connect()
+        {
+            string connectionServer = "server=localhost;port=3306;database=placemybet;uid=root;password=maribel";
+            MySqlConnection connection = new MySqlConnection(connectionServer);
+            return connection;
+        }
+
         internal Users Retrieve()
         {
-            Users user = new Users("prueba@gmail.com", "Pepe", "Martínez", 25);
+            //Users user = new Users("prueba@gmail.com", "Pepe", "Martínez", 25);
 
+            MySqlConnection connection = Connect();
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "select * from usuario";
+
+            connection.Open();
+            MySqlDataReader result = command.ExecuteReader();
+
+            Users user = null;
+
+            if (result.Read())
+            {
+                Debug.WriteLine("Recuparado:" + result.GetString(0) + " " + result.GetString(1) + " " + result.GetString(2) + " " + result.GetInt32(3));
+                user = new Users(result.GetString(0), result.GetString(1), result.GetString(2), result.GetInt32(3));
+            }
+
+            connection.Close();
             return user;
         }
     }
